551fa6001801f7180ec8caaf2b1009e5cf49682e
Clark Hinchcliff <clarkhinchcliff@hotmail.com>
2014-05-03 18:40:20 -0600

    Updates, SQL and Ruby Triangle Problem

--- week_8_and_9/4_Ruby/triangle_debugger/my_solution.rb
+++ week_8_and_9/4_Ruby/triangle_debugger/my_solution.rb
@@ -6,24 +6,51 @@
 
 # 1. Original Solution
 
-def valid_triangle?(a, b, c, sum)
+# def valid_triangle?(a, b, c, sum)
+#   if a != 0 || b != 0 || c != 0
+#   if a >= b
+#   largest = a
+#   sum += b
+#   else largest = b
+#   sum += a
+#   end
+#   if c > largest
+#   sum += largest
+#   largest = c
+#   else sum += c
+#   end
+#   if sum > largest
+#   return "true"
+#   else return "false"
+#   end
+#   else return "false"
+#   end
+# end
+
+def valid_triangle?(a, b, c)
+  sum = 0
+  return false if a <= 0 || b <= 0 || c <= 0
+  return false if a + b < c || a + c < b || b + c < a
   if a != 0 || b != 0 || c != 0
-  if a >= b
-  largest = a
-  sum += b
-  else largest = b
-  sum += a
-  end
-  if c > largest
-  sum += largest
-  largest = c
-  else sum += c
-  end
-  if sum > largest
-  return "true"
-  else return "false"
-  end
-  else return "false"
+    if a >= b
+      largest = a
+      sum += b
+    else largest = b
+      sum += a
+    end
+    if c > largest
+      sum += largest
+      largest = c
+    else 
+      sum += c
+    end
+    if sum > largest
+      return "true"
+    else 
+      return "false"
+    end
+  else 
+    return "false"
   end
 end
 
@@ -32,30 +59,163 @@ end
 
 # --- Bug 1 ---
 # * what is the exact description of the error?
+#   valid_triangle? expects 3 arguments
 # * what is the exact line number the error is appearing?
+#    line 9
 # * before you fix the bug, what do you think is causing the error?
+#   the sum arguement 
 
 # --- Bug 2 ---
 # * what is the exact description of the error?
+#   valid_triangle? returns false if any of the arguments are 0
 # * what is the exact line number the error is appearing?
+#   line 20
 # * before you fix the bug, what do you think is causing the error?
 
 # --- Bug 3 ---
 # * what is the exact description of the error?
+#   valid_triangle? returns false for any impossible triangle
 # * what is the exact line number the error is appearing?
+#   line 46 in spec file (not returning expected value)
 # * before you fix the bug, what do you think is causing the error?
-
+#   It's because we don't have any test to check for an invalid traingle (a + b > c)
 
 # 3. Refactored Solution (Comment the other code to run this)
 
-
+def valid_triangle?(a, b, c)
+  a + b > c && a + c > b && b + c > a ? true : false
+end
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+def assert
+  raise "assertion failed" unless yield 
+end
+
+assert { valid_triangle?(0,2,2) == false }
+assert { valid_triangle?(2,2,2) == true }
+assert { valid_triangle?(1,2,3) == false }
 
 
+# U3.W8-9: Triangle Debugger
 
 
+# I worked on this challenge [by myself, with: ].
+
+
+# 1. Original Solution
+
+# def valid_triangle?(a, b, c, sum)
+#   if a != 0 || b != 0 || c != 0
+#   if a >= b
+#   largest = a
+#   sum += b
+#   else largest = b
+#   sum += a
+#   end
+#   if c > largest
+#   sum += largest
+#   largest = c
+#   else sum += c
+#   end
+#   if sum > largest
+#   return "true"
+#   else return "false"
+#   end
+#   else return "false"
+#   end
+# end
+
+def valid_triangle?(a, b, c)
+  sum = 0
+  return false if a <= 0 || b <= 0 || c <= 0
+  return false if a + b < c || a + c < b || b + c < a
+  if a != 0 || b != 0 || c != 0
+    if a >= b
+      largest = a
+      sum += b
+    else largest = b
+      sum += a
+    end
+    if c > largest
+      sum += largest
+      largest = c
+    else 
+      sum += c
+    end
+    if sum > largest
+      return "true"
+    else 
+      return "false"
+    end
+  else 
+    return "false"
+  end
+end
+
+
+# 2. Answer the question for each bug
+
+# --- Bug 1 ---
+# * what is the exact description of the error?
+#   valid_triangle? expects 3 arguments
+# * what is the exact line number the error is appearing?
+#    line 9
+# * before you fix the bug, what do you think is causing the error?
+#   the sum arguement 
+
+# --- Bug 2 ---
+# * what is the exact description of the error?
+#   valid_triangle? returns false if any of the arguments are 0
+# * what is the exact line number the error is appearing?
+#   line 20
+# * before you fix the bug, what do you think is causing the error?
+
+# --- Bug 3 ---
+# * what is the exact description of the error?
+#   valid_triangle? returns false for any impossible triangle
+# * what is the exact line number the error is appearing?
+#   line 46 in spec file (not returning expected value)
+# * before you fix the bug, what do you think is causing the error?
+#   It's because we don't have any test to check for an invalid traingle (a + b > c)
+
+# 3. Refactored Solution (Comment the other code to run this)
+
+def valid_triangle?(a, b, c)
+  a + b > c && a + c > b && b + c > a ? true : false
+end
+
+
+# 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+
+def assert
+  raise "assertion failed" unless yield 
+end
+
+assert { valid_triangle?(0,2,2) == false }
+assert { valid_triangle?(2,2,2) == true }
+assert { valid_triangle?(1,2,3) == false }
 
 # 5. Reflection 
+
+# What parts of your strategy worked? What problems did you face?
+#We had an easy time with this challenge. It was fun. We realized that if you put 0 anywhere in our code it would come out to being false, so we only needed one line of code.
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+  #Getting arity to work with methods is different from classes, that's something I'll need to research.
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+  #Not really any, I just need to review some things.
+
+# Did you learn any new skills or tricks?
+  #Nope
+
+# How confident are you with each of the Learning Competencies?
+  #Pretty confident, ruby isn't hard to work with
+
+# Which parts of the challenge did you enjoy?
+  #All really, I love working with ruby
+
+# Which parts of the challenge did you find tedious?
+  #none
--- week_8_and_9/5_SQL/your_own_db/my_solution.md
+++ week_8_and_9/5_SQL/your_own_db/my_solution.md
@@ -1,1 +1,36 @@
 <!-- ## Show the terminal output here.  -->
+
+sqlite> SELECT * FROM blog;
+id          user_id     created_at           updated_at         
+----------  ----------  -------------------  -------------------
+1           1           2014-05-03 22:35:49  2014-05-03 22:35:49
+
+sqlite> SELECT * FROM blog_entry
+   ...> ;
+id          blog_id     entry_name  entry       created_at           updated_at         
+----------  ----------  ----------  ----------  -------------------  -------------------
+1           1           one         stuff       2014-05-03 22:45:40  2014-05-03 22:45:40
+2           1           two         more stuff  2014-05-03 23:49:30  2014-05-03 23:49:30
+3           1           three       even more   2014-05-03 23:51:50  2014-05-03 23:51:50
+4           1           four        lots of it  2014-05-03 23:55:52  2014-05-03 23:55:52
+5           1           five        blarg       2014-05-04 00:01:56  2014-05-04 00:01:56
+6           1           six         sturff      2014-05-04 00:02:49  2014-05-04 00:02:49
+7           1           seven       stump       2014-05-04 00:03:22  2014-05-04 00:03:22
+8           1           eight       struff      2014-05-04 00:03:52  2014-05-04 00:03:52
+9           1           nine        struuff     2014-05-04 00:04:24  2014-05-04 00:04:24
+10          1           ten         too much s  2014-05-04 00:05:08  2014-05-04 00:05:08
+
+sqlite> SELECT entry_name, entry FROM blog_entry AS blog_entry JOIN blog
+   ...> AS blog ON blog_entry.blog_id=blog.id;
+entry_name  entry     
+----------  ----------
+one         stuff     
+two         more stuff
+three       even more 
+four        lots of it
+five        blarg     
+six         sturff    
+seven       stump     
+eight       struff    
+nine        struuff   
+ten         too much s
